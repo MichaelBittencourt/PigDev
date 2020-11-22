8c3aff94ec660cc4fc300ef1a4e5ac61c9939de6
Michael Bittencourt <mchl.bittencourt@gmail.com>
2020-11-22 01:16:11 -0400

    Create a simple example code

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,64 @@
 #include "PIG.h"
 
-PIG_Evento evento;          //evento ser tratadoi a cada pssada do loop principal
-PIG_Teclado meuTeclado;     //variável como mapeamento do teclado
+PIG_Evento event;          //evento ser tratadoi a cada pssada do loop principal
+PIG_Teclado myKeyboard;     //variável como mapeamento do teclado
+double posX = 100, posY = 100;
+double speed = 0.01;
+
+void updateObject();
 
 int main( int argc, char* args[] ){
+
 
     //criando o jogo (aplicação)
     CriaJogo("Meu Jogo");
 
     //associando o teclado (basta uma única vez) com a variável meuTeclado
-    meuTeclado = GetTeclado();
+    myKeyboard = GetTeclado();
 
     //loop principal do jogo
     while(JogoRodando()){
 
         //pega um evento que tenha ocorrido desde a última passada do loop
-        evento = GetEvento();
+        event = GetEvento();
 
         //aqui o evento deve ser tratado e as coisas devem ser atualizadas
 
         //será feita aprepação do frame que será exibido na tela
         IniciaDesenho();
 
-        //todas as chamadas de desenho devem ser feitas aqui na ordem desejada
+        
+        updateObject();
 
+        DesenhaRetangulo(40, 40, 100, 100, {255, 0, 0, 50});
         //o frame totalmente pronto será mostrado na tela
         EncerraDesenho();
+
+        if(myKeyboard[TECLA_ESC]) {
+            break;
+        }
     }
 
     //o jogo será encerrado
     FinalizaJogo();
 
     return 0;
+}
+
+void updateObject() {
+    if(myKeyboard[TECLA_a]) {
+        posX -= speed;
+    }
+    if(myKeyboard[TECLA_d]) {
+        posX += speed;
+    }
+    if(myKeyboard[TECLA_w]) {
+        posY += speed;
+    }
+    if(myKeyboard[TECLA_s]) {
+        posY -= speed;
+    }
+
+    DesenhaRetangulo(posX, posY, 100, 100, VERDE);
 }
 
